3ba91f2cf64c7c330ffaa8381c1c00693d93144d
Om Dalavi <omdalvi184@gmail.com>
2023-03-12 01:03:28 +0530

    Final Commit

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://astrovnit.netlify.app",
+    origin: "http://astrovnit.in",
   })
 );
 app.use("/user", userRoutes);
